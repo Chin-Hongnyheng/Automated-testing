c82c6f812b362fe68955e4bad0d7a6e687beb34b
Chin Hongnyheng <chinhongnyheng@Chins-MacBook-Pro.local>
2026-06-29 14:59:55 +0700

    Add Dependencies

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,12 @@
+package test.java;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void testIt() {
+        assertTrue(2 * 3 == 6);
+    }
+}
